9cd1143d4255dac53f2b820acc9dc10dcbdd7831
mu88 <4560672+mu88@users.noreply.github.com>
2021-07-30 15:19:32 +0200

    Create domain model

---
+++ src/Entities/Creator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Creator
+    {
+        public Creator(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ src/Entities/LifePoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Entities
+{
+    public class LifePoint
+    {
+        public LifePoint(Guid id,
+                         DateTime date,
+                         string caption,
+                         string description,
+                         double latitude,
+                         double longitude,
+                         Creator createdBy)
+        {
+            Id = id;
+            Date = date;
+            Caption = caption;
+            Description = description;
+            Latitude = latitude;
+            Longitude = longitude;
+            CreatedBy = createdBy;
+        }
+
+        public Guid Id { get; }
+
+        public DateTime Date { get; }
+
+        public string Caption { get; }
+
+        public string Description { get; }
+
+        public double Latitude { get; }
+
+        public double Longitude { get; }
+
+        public Creator CreatedBy { get; }
+    }
+}
