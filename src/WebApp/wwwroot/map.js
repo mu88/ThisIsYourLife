aecaeb397bb7aee621215778d8e43309e446f79d
mu88 <4560672+mu88@users.noreply.github.com>
2021-08-02 12:54:50 +0200

    Implement basic map behavior

---
+++ src/WebApp/wwwroot/map.js
@@ -0,0 +1,11 @@
+﻿export function initializeMap() {
+    var myMap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+    return myMap;
+}
+
+export function addMarker(myMap, longitude, latitude) {
+    L.marker([longitude, latitude]).addTo(myMap);
+}
