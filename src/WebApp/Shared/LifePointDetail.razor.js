ed05d5586d6647b1e64c736fa6c09c19329cd2ea
mu88 <4560672+mu88@users.noreply.github.com>
2022-04-30 13:18:28 +0200

    Fix bug when deleting arbitrary LifePoint

--- src/WebApp/Shared/LifePointDetail.razor.js
+++ src/WebApp/Shared/LifePointDetail.razor.js
@@ -1,11 +1,21 @@
-﻿let _marker;
+﻿let _leafletMap;
+let _markers = []
 
 export function createMarkerForExistingLifePoint(leafletMap, id, latitude, longitude) {
-    _marker = L.marker([latitude, longitude]);
-    _marker.bindPopup("<life-point-detail id='" + id + "'></life-point-detail>", { minWidth: 500 });
-    _marker.addTo(leafletMap);
+    _leafletMap = leafletMap;
+    let marker = L.marker([latitude, longitude]);
+    marker._id = id;
+    marker.bindPopup("<life-point-detail id='" + id + "'></life-point-detail>", {minWidth: 500});
+    marker.addTo(leafletMap);
+    _markers.push(marker);
 }
 
-export function removeMarkerOfLifePoint() {
-    _marker.remove();
+export function removeMarkerOfLifePoint(id) {
+    // https://stackoverflow.com/questions/45931963/leaflet-remove-specific-marker
+    let new_markers = [];
+    _markers.forEach(function (marker) {
+        if (marker._id === id) _leafletMap.removeLayer(marker);
+        else new_markers.push(marker);
+    })
+    _markers = new_markers;
 }
