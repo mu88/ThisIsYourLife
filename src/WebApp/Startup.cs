b9668fc833e3a5bfa2b1dc60fce3f6f9fa6c7c20
mu88 <4560672+mu88@users.noreply.github.com>
2022-04-30 16:38:15 +0200

    Use HTTP instead of HTTPS

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -48,14 +48,8 @@ public class Startup
 
         if (env.IsDevelopment())
             app.UseDeveloperExceptionPage();
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+        else { app.UseExceptionHandler("/Error"); }
 
-        app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
