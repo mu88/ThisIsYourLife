7660dfd41d726c24e1943aeadc4664706bdde511
mu88 <4560672+mu88@users.noreply.github.com>
2022-05-08 13:43:29 +0200

    feature: increase text size for new LifePoint

--- src/WebApp/Models/NewLifePointModel.cs
+++ src/WebApp/Models/NewLifePointModel.cs
@@ -7,11 +7,11 @@ namespace WebApp.Models;
 public class NewLifePointModel
 {
     [Required(ErrorMessageResourceName = "RequiredError", ErrorMessageResourceType = typeof(Main))]
-    [StringLength(30, MinimumLength = 1, ErrorMessageResourceName = "LengthError", ErrorMessageResourceType = typeof(Main))]
+    [StringLength(50, MinimumLength = 1, ErrorMessageResourceName = "LengthError", ErrorMessageResourceType = typeof(Main))]
     public string Caption { get; set; } = string.Empty;
 
     [Required(ErrorMessageResourceName = "RequiredError", ErrorMessageResourceType = typeof(Main))]
-    [StringLength(200, MinimumLength = 1, ErrorMessageResourceName = "LengthError", ErrorMessageResourceType = typeof(Main))]
+    [StringLength(500, MinimumLength = 1, ErrorMessageResourceName = "LengthError", ErrorMessageResourceType = typeof(Main))]
     public string Description { get; set; } = string.Empty;
 
     [Required(ErrorMessageResourceName = "RequiredError", ErrorMessageResourceType = typeof(Main))]
