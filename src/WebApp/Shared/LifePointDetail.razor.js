30085197c12e1ff435774da69a88fba60a23129d
mu88 <4560672+mu88@users.noreply.github.com>
2022-05-13 12:01:22 +0200

    feature: cluster markers in groups

--- src/WebApp/Shared/LifePointDetail.razor.js
+++ src/WebApp/Shared/LifePointDetail.razor.js
@@ -1,12 +1,16 @@
-﻿let _leafletMap;
-let _markers = []
+﻿let _markers = []
+let _markerClusterGroup;
 
 export function createMarkerForExistingLifePoint(leafletMap, id, latitude, longitude) {
-    _leafletMap = leafletMap;
+    if (!_markerClusterGroup) {
+        _markerClusterGroup = L.markerClusterGroup();
+        leafletMap.addLayer(_markerClusterGroup);
+    }
+
     let marker = L.marker([latitude, longitude]);
     marker._id = id;
     marker.bindPopup("<life-point-detail id='" + id + "'></life-point-detail>", {minWidth: 500});
-    marker.addTo(leafletMap);
+    _markerClusterGroup.addLayer(marker);
     _markers.push(marker);
 }
 
@@ -14,8 +18,9 @@ export function removeMarkerOfLifePoint(id) {
     // https://stackoverflow.com/questions/45931963/leaflet-remove-specific-marker
     let new_markers = [];
     _markers.forEach(function (marker) {
-        if (marker._id === id) _leafletMap.removeLayer(marker);
-        else new_markers.push(marker);
+        if (marker._id === id) {
+            _markerClusterGroup.removeLayer(marker);
+        } else new_markers.push(marker);
     })
     _markers = new_markers;
 }
