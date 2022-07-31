b4982790e0b1e67a3aabc8a1cf4a132762b327f3
mu88 <4560672+mu88@users.noreply.github.com>
2022-07-31 16:21:08 +0200

    refactor: move code into dedicated files to avoid flaky code cleanup in Rider

---
+++ src/WebApp/Shared/Map.razor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DTO.Location;
+using DTO.Person;
+using Logging.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WebApp.Shared;
+
+public partial class Map
+{
+    private IJSObjectReference _mapModule = null!;
+    private IJSObjectReference _newLifePointModule = null!;
+    private IJSObjectReference _lifePointDetailModule = null!;
+    private IJSObjectReference _leafletMap = null!;
+    private IReadOnlyList<ExistingLocation> _allLocations = new List<ExistingLocation>();
+    private DotNetObjectReference<Map>? _objRef;
+    private IEnumerable<ExistingPerson> _distinctCreators = new List<ExistingPerson>();
+    private IEnumerable<int> _distinctYears = new List<int>();
+
+    [JSInvokable]
+    public async Task OpenPopupForNewLifePointAsync(double latitude, double longitude)
+    {
+        Logger.MethodStarted();
+        await _newLifePointModule.InvokeVoidAsync("createPopupForNewLifePoint", _objRef, _leafletMap, latitude, longitude);
+        Logger.MethodFinished();
+    }
+
+    [JSInvokable]
+    public async Task AddMarkerAsync(Guid id, double latitude, double longitude)
+        => await _lifePointDetailModule.InvokeVoidAsync("createMarkerForExistingLifePoint", _leafletMap, id, latitude, longitude);
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Logger.MethodStarted();
+            _objRef = DotNetObjectReference.Create(this);
+            await InitializeMapAsync(_objRef);
+            await AddMarkersForExistingLocationsAsync();
+            Logger.MethodFinished();
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void OnInitialized()
+    {
+        Logger.MethodStarted();
+
+        base.OnInitialized();
+
+        _distinctCreators = LifePointService.GetDistinctCreators();
+        _distinctYears = LifePointService.GetDistinctYears();
+
+        Logger.MethodFinished();
+    }
+
+    private async Task InitializeMapAsync(DotNetObjectReference<Map> dotNetObjectReference)
+    {
+        _mapModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Shared/Map.razor.js");
+        _leafletMap = await _mapModule.InvokeAsync<IJSObjectReference>("initializeMap", 51.0405849, 13.7478431, 20, dotNetObjectReference);
+
+        _newLifePointModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Shared/NewLifePoint.razor.js");
+        _lifePointDetailModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Shared/LifePointDetail.razor.js");
+    }
+
+    private async Task AddMarkersForExistingLocationsAsync()
+    {
+        Logger.MethodStarted();
+
+        foreach (var (latitude, longitude, id) in LifePointService.GetAllLocations()) { await AddMarkerAsync(id, latitude, longitude); }
+
+        Logger.MethodFinished();
+    }
+
+    private void SelectedCreatorChanged(ChangeEventArgs args)
+    {
+        // TODO mu88: Redraw
+    }
+
+    private void SelectedYearChanged(ChangeEventArgs args)
+    {
+        // TODO mu88: Redraw
+    }
+
+    private void OnUserDialogClose() => StateHasChanged();
+}
---
+++ src/WebApp/Shared/NewLifePoint.razor.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DTO.LifePoint;
+using Logging.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using Persistence;
+using WebApp.Models;
+
+namespace WebApp.Shared;
+
+public partial class NewLifePoint
+{
+    internal const long MaxAllowedFileSizeInBytes = MaxAllowedFileSizeInMegaBytes * 1024 * 1024;
+    private const long MaxAllowedFileSizeInMegaBytes = 15;
+    internal bool ImageTooBig;
+    internal bool InputIsNoImage;
+    private readonly NewLifePointModel _newLifePoint = new();
+    private IJSObjectReference _newLifePointModule = null!; // is initialized on component construction 
+    private IBrowserFile? _file;
+    private bool _showModalSpinner;
+
+    [Parameter]
+    public double Latitude { get; set; }
+
+    [Parameter]
+    public double Longitude { get; set; }
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadNewLifePointModuleAsync();
+
+        await base.OnInitializedAsync();
+
+        _newLifePoint.Date = NewLifePointDateService.ProposedCreationDate;
+    }
+
+    /// <inheritdoc />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (!firstRender) await UpdatePopupAsync();
+    }
+
+    private async Task CreateNewLifePointAsync()
+    {
+        Logger.MethodStarted();
+
+        EnableSpinner();
+
+        ImageToCreate? imageToCreate;
+        try
+        {
+            ImageTooBig = false;
+            imageToCreate = _file != null ? new ImageToCreate(_file.OpenReadStream(MaxAllowedFileSizeInBytes)) : null;
+        }
+        catch (IOException)
+        {
+            Logger.ImageTooBig();
+            DisableSpinner();
+            ImageTooBig = true;
+            return;
+        }
+
+        var lifePointToCreate = new LifePointToCreate(_newLifePoint.Date,
+                                                      _newLifePoint.Caption,
+                                                      _newLifePoint.Description,
+                                                      Latitude,
+                                                      Longitude,
+                                                      UserService.Id ?? throw new NullReferenceException(Loc["UserHasNotBeenSet"]),
+                                                      imageToCreate);
+
+        ExistingLifePoint createdLifePoint;
+        try
+        {
+            createdLifePoint = await LifePointService.CreateLifePointAsync(lifePointToCreate);
+            InputIsNoImage = false;
+        }
+        catch (NoImageException)
+        {
+            DisableSpinner();
+            InputIsNoImage = true;
+            return;
+        }
+
+        NewLifePointDateService.ProposedCreationDate = _newLifePoint.Date;
+
+        DisableSpinner();
+
+        await RemovePopupAsync();
+        await AddMarkerAsync(createdLifePoint);
+
+        Logger.MethodFinished();
+    }
+
+    private void EnableSpinner()
+    {
+        _showModalSpinner = true;
+        StateHasChanged();
+    }
+
+    private void DisableSpinner()
+    {
+        _showModalSpinner = false;
+        StateHasChanged();
+    }
+
+    private async Task RemovePopupAsync()
+    {
+        Logger.MethodStarted();
+
+        await _newLifePointModule.InvokeVoidAsync("removePopupForNewLifePoint");
+
+        Logger.MethodFinished();
+    }
+
+    private async Task AddMarkerAsync(ExistingLifePoint existingLifePoint)
+    {
+        Logger.MethodStarted();
+
+        await _newLifePointModule.InvokeVoidAsync("addMarkerForCreatedLifePoint", existingLifePoint.Id, existingLifePoint.Latitude, existingLifePoint.Longitude);
+
+        Logger.MethodFinished();
+    }
+
+    private async Task LoadNewLifePointModuleAsync() => _newLifePointModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Shared/NewLifePoint.razor.js");
+
+    private void LoadImage(InputFileChangeEventArgs args) => _file = args.File;
+
+    /// <summary>Updates the underlying Leaflet popup so that size and position are correct and visible.</summary>
+    /// <remarks>
+    ///     Since the Blazor component is rendered lazily (and therefore after the Leaflet popup has been created),
+    ///     the popup has to be updated manually. This will resize it according to the Blazor content and
+    ///     also pan the map so that it becomes visible.
+    ///     If an image is present, this must happen after the image is loaded - otherwise the calculated size of
+    ///     the popup is wrong and would therefore be displayed at the wrong map position.
+    /// </remarks>
+    private async Task UpdatePopupAsync()
+    {
+        if (_newLifePointModule == null!) return;
+
+        await _newLifePointModule.InvokeVoidAsync("updatePopup");
+    }
+}
