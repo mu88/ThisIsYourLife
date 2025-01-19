442f6b31836675f856863f4d35c01893cb63cd96
mu88 <4560672+mu88@users.noreply.github.com>
2025-01-19 18:47:23 +0100

    refactor(test): avoid using HTTP assertions from FluentAssertions as they will be gone with v8

--- src/Tests/System/SystemTests.cs
+++ src/Tests/System/SystemTests.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Networks;
@@ -93,13 +94,13 @@ public class SystemTests
     
     private static async Task AppShouldRunAsync(HttpResponseMessage appResponse, CancellationToken cancellationToken)
     {
-        appResponse.Should().BeSuccessful();
+        appResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         (await appResponse.Content.ReadAsStringAsync(cancellationToken)).Should().Contain("<title>This is your life</title>");
     }
 
     private static async Task HealthCheckShouldBeHealthyAsync(HttpResponseMessage healthCheckResponse, CancellationToken cancellationToken)
     {
-        healthCheckResponse.Should().BeSuccessful();
+        healthCheckResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         (await healthCheckResponse.Content.ReadAsStringAsync(cancellationToken)).Should().Be("Healthy");
     }
 
