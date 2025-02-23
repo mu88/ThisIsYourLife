d726e5624771a9d9c9cb4e9fd027c2892408320d
mu88 <4560672+mu88@users.noreply.github.com>
2025-02-23 16:35:03 +0100

    chore: resolve Sonar issue by removing orphaned to-do

--- src/WebApp/Shared/Map.razor.js
+++ src/WebApp/Shared/Map.razor.js
@@ -4,34 +4,33 @@ let _dotNetMapReference;
 let _leafletMap;
 
 export function initializeMap(startLongitude, startLatitude, startZoom, dotNetMapReference) {
-    _dotNetMapReference = dotNetMapReference;
-    _leafletMap = L.map("mapid").setView([startLongitude, startLatitude], startZoom);
-    L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
-    }).addTo(_leafletMap);
+  _dotNetMapReference = dotNetMapReference;
+  _leafletMap = L.map("mapid").setView([startLongitude, startLatitude], startZoom);
+  L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+  }).addTo(_leafletMap);
 
-    _createFilterLifePointsCommand().addTo(_leafletMap);
+  _createFilterLifePointsCommand().addTo(_leafletMap);
 
-    _leafletMap.on("dblclick", _onMapDoubleClick);
+  _leafletMap.on("dblclick", _onMapDoubleClick);
 
-    return _leafletMap;
+  return _leafletMap;
 }
 
 function _onMapDoubleClick(e) {
-    // TODO mu88: Handle 'Abort/Close'
-    let latitude = e.latlng.lat;
-    let longitude = e.latlng.lng;
-    _dotNetMapReference.invokeMethodAsync("OpenPopupForNewLifePointAsync", latitude, longitude);
+  let latitude = e.latlng.lat;
+  let longitude = e.latlng.lng;
+  _dotNetMapReference.invokeMethodAsync("OpenPopupForNewLifePointAsync", latitude, longitude);
 }
 
 function _createFilterLifePointsCommand() {
-    let command = L.control({position: "topright"});
-    command.onAdd = function () {
-        let div = L.DomUtil.create("div", "command");
-        div.innerHTML = "<filter-life-points/>";
-        L.DomUtil.addClass(div, "filter-div");
-        return div;
-    };
+  let command = L.control({position: "topright"});
+  command.onAdd = function () {
+    let div = L.DomUtil.create("div", "command");
+    div.innerHTML = "<filter-life-points/>";
+    L.DomUtil.addClass(div, "filter-div");
+    return div;
+  };
 
-    return command;
+  return command;
 }
--- src/WebApp/Shared/NewLifePoint.razor.js
+++ src/WebApp/Shared/NewLifePoint.razor.js
@@ -4,31 +4,31 @@ let _popup;
 let _dotNetMapReference;
 
 export function createPopupForNewLifePoint(dotNetMapReference, leafletMap, latitude, longitude) {
-    _dotNetMapReference = dotNetMapReference;
+  _dotNetMapReference = dotNetMapReference;
   _popup = L.popup({minWidth: calculateTooltipWidth(), closeOnEscapeKey: false, closeOnClick: false})
-        .setLatLng([latitude, longitude])
-        .setContent("<new-life-point latitude='" + latitude + "' longitude='" + longitude + "'></new-life-point>");
-    _popup.openOn(leafletMap);
+    .setLatLng([latitude, longitude])
+    .setContent("<new-life-point latitude='" + latitude + "' longitude='" + longitude + "'></new-life-point>");
+  _popup.openOn(leafletMap);
 }
 
 export function removePopupForNewLifePoint() {
-    _popup.remove();
+  _popup.remove();
 }
 
 export function addMarkerForCreatedLifePoint(id, latitude, longitude) {
-    _dotNetMapReference.invokeMethodAsync("AddMarkerAsync", id, latitude, longitude);
+  _dotNetMapReference.invokeMethodAsync("AddMarkerAsync", id, latitude, longitude);
 }
 
 export function updatePopup() {
-    if (!_popup._map) {
-        return;
-    }
+  if (!_popup._map) {
+    return;
+  }
 
-    // the following code is borrowed from the original Leaflet sources
-    // /src/layer/DivOverlay.js (base class of popup), update().
-    // Make sure that this._updateContent() doesn't get called, because otherwise there'll be an infinite loop
-    // between updating the popup and recreating the Blazor component. 
-    _popup._updateLayout();
-    _popup._updatePosition();
-    _popup._adjustPan();
+  // the following code is borrowed from the original Leaflet sources
+  // /src/layer/DivOverlay.js (base class of popup), update().
+  // Make sure that this._updateContent() doesn't get called, because otherwise there'll be an infinite loop
+  // between updating the popup and recreating the Blazor component. 
+  _popup._updateLayout();
+  _popup._updatePosition();
+  _popup._adjustPan();
 }
